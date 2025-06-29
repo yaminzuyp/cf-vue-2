ca2905b70d000b14e188c957cf802a57cc40b906
yaminzuyp <yzainiyah@gmail.com>
2025-06-29 19:59:46 +0700

    setting backend

--- api/index.js
+++ api/index.js
@@ -1,12 +1,12 @@
-// api/index.js
 import { Hono } from 'hono'
 import { serveStatic } from 'hono/cloudflare-workers'
-import { html } from 'hono/html'
 
 const app = new Hono()
 
-app.use('*', serveStatic({ root: './' }))
+// Serve static files (Vue dist)
+app.use('*', serveStatic({ root: './dist' }))
 
+// API endpoint contoh
 app.get('/api/hello', (c) => {
   return c.json({ message: 'Hello from Hono!' })
 })
